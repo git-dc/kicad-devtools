3336d0c6eafa70a639a1dead5d9e89ae01875fd7
Daniel Chirita <dschirita@gmail.com>
2024-06-24 15:03:39 -0700

    dynamically find the name of the project from the gggparent direction name and put it in the name of the output archive

--- gbr_packager/gbr_pkger.sh
+++ gbr_packager/gbr_pkger.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 p=$(dirname $PWD)
+ggp=$(dirname $(dirname $(dirname $PWD)))
 parent=${PWD##*/}
 grandparent=${p##*/}
-fname="$PWD/../../fab+assy_"$grandparent"_"$parent".zip"
+gggparent=${ggp##*/}
+fname="$PWD/../../"$gggparent"_"$grandparent"_"$parent".zip"
 echo "Packaging gerbers and BOMs into $fname"
 cp $PWD/../../bom_sorted.csv $PWD/bom_sorted_"$grandparent"_"$parent".csv
 cp $PWD/../../bom_grouped.csv $PWD/bom_grouped_"$grandparent"_"$parent".csv
